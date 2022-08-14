f0309c1ad8969f78ac82dfe1de9dd191d6554113
naoisobe <ganmodoki7.43dari@gmail.com>
2022-08-14 22:11:16 +0900

    connect rails and nextjs

--- project-backend/config/environments/development.rb
+++ project-backend/config/environments/development.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << 'localhost'
+  # config.hosts << 'localhost'
   config.hosts << '127.0.0.1'
-  config.hosts << ENV['BACKEND_HOST']
+  config.hosts << "backend"
+  # config.hosts << ENV['BACKEND_HOST']
 end
